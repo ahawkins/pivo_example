16b7748dbffba2e9d2fa163f850cc75471104e83
ahawkins <adam@hawkins.io>
2013-10-30 01:17:45 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pankki::Application.config.secret_key_base = '750d8a5f7aaa9e0528ddba0dbcb076faab4e86da279524b63a0358b542e31c0e16b0b4a1c77989c85aeef0c69e88e153d9dea9581468b9824bb508c8e6ded8ad'
